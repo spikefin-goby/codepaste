7efe97d86457a5b252f6724f0730427dca5ee298
hirose <superblueplanet@gmail.com>
2014-01-19 00:15:29 +0900

    create code show function.

--- webroot/index.php
+++ webroot/index.php
@@ -1,23 +1,48 @@
 <?php
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use KzykHys\Pygments\Pygments;
 
 $app = new Silex\Application();
 $app['debug'] = true;
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-	'twig.path' => __DIR__ . '/../views',
+    'twig.path' => __DIR__ . '/../views',
 ));
 
-$app->get('/', function() use($app) {
-	return $app['twig']->render('codepaste/index.html.twig');
+/**
+ * index
+ */
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('codepaste/index.html.twig');
 });
 
+/**
+ * code paste
+ */
+$app->post('/paste', function (Request $request) use ($app) {
+    $code = $request->get('code');
+    if (!$code) {
+        return new Response('Missing parameters.', 400);
+    }
 
-$app->get('/show/{hash}', function($hash) use ($app) {
-	return $app['twig']->render('codepaste/show.html.twig');
+    $hash = md5($code);
+    
+
 });
 
-$app->get('/hello/{name}', function($name) use ($app) {
-	return 'Hello '.$app->escape($name);
+/**
+ * code show
+ */
+$app->get('/show/{hash}', function ($hash) use ($app) {
+    $pygments = new Pygments();
+    $code = file_get_contents('/Users/hirose/github/codepaste/webroot/index.php');
+    $code_highlight = $pygments->highlight($code, null, 'html', array('linenos' => 1));
+
+    return $app['twig']->render('codepaste/show.html.twig', array(
+        'code_highlight' => $code_highlight,
+    ));
 });
 
 $app->run();
